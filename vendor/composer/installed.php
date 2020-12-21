422afc013237c9a83049db661f08e04919c046d1
Martin Darras <darras.martin@gmail.com>
2020-12-21 17:52:58 +0100

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9b31298016abb7d7c6f27b734a9af7626920eb4d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9b31298016abb7d7c6f27b734a9af7626920eb4d',
+    ),
+  ),
+);
